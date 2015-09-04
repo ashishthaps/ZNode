8793bc1c5d9d73d6aea9c90afa8f5a52cb6e8568
ashish thapliyal <ashishthaps@hotmail.com>
2015-09-04 15:30:33 -0700

    some update

--- ZNode/server.js
+++ ZNode/server.js
@@ -2,6 +2,6 @@
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World from docker container\n');
+    res.end('Hello World from docker container via codeship\n');
 }).listen(port);
 
